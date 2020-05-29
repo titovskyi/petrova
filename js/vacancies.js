3e98765f23d4fa1cdf342f9d06ec54ec026de226
markDrigola <markdrigola2802@gmail.com>
2020-05-29 20:35:54 +0300

    next bags fix

--- js/vacancies.js
+++ js/vacancies.js
@@ -73,14 +73,14 @@ let typedArr = [{ element: $('.typed-slide-1'),
 
         typedElement.typed({
             strings: config.data,
-            typeSpeed: 50,
+            typeSpeed: 35,
             startDelay: 0,
             callback: function () {
                 $(typedElement).html(parseJsonToHtml(typedElement.text()));
 
                 interval = setTimeout(function() {
                     vacSlide.slick('slickNext');
-                }, 3000);
+                }, 2600);
             }
         });
     }
